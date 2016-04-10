f65aa03b66452219a2f86e712a1f30f3661bf18e
lucasmarcelli <lucas@marcelli.ca>
2016-04-09 20:57:24 -0400

    Added a news page. Fixes #13

--- mood/admin.py
+++ mood/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 	
-from .models import Day, Entry
+from .models import Day, Entry, News
 
 admin.site.register(Day)
 admin.site.register(Entry)
+admin.site.register(News)
